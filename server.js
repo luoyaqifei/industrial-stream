140e48cccc97e66907982cd7324e77c3ec0e45d9
Jingwen He <JHe15@slb.com>
2018-06-13 10:29:26 +0800

    backendv1

--- server.js
+++ server.js
@@ -9,65 +9,24 @@ const bodyParser = require('body-parser'),
     config = require('./config'),
     _ = require('lodash'),
     argv = require('yargs').argv;
-// import {fetchToken, isTokenExpired} from "./tools/tokenTool";
 
 let port = argv.port || 3003;
 
 const proxy = require('http-proxy-middleware');
-// const activityCodeJson = require(path.join(__dirname, 'ActivityCode.json'));
-
-// const sendTokenParams = {
-//   'NameIdentifier': 'haha',
-//   'unique_name': 'haha',
-//   'grant_type': 'password'
-// };
 
 // let token = null;
 app.use(morgan('dev'));
 app.use(cors());
-
-// app.get('/activity-code', (req, res) => {
-//   let catalogId = req.query.catalog_uid;
-//   let item = _.find(activityCodeJson.codes, i => i.catalog_uid === catalogId);
-//   if (!_.isNil(item)) {
-//     res.send(item.name);
-//   }
-//   else {
-//     res.send(null);
-//   }
-// });
-
-// app.get('/token', (req, res) => {
-//   if (!token || isTokenExpired(token)) {
-//     Promise.resolve(fetchToken(sendTokenParams))
-//       .then((data) => {
-//         token = data;
-//         res.send(data);
-//       });
-//     return res;
-//   }
-//   else {
-//     res.send(token);
-//     return res;
-//   }
-// });
-
-// app.use('/api', proxy(
-//   {
-//     target: `http://localhost:${config.plannerPort}/aiplanner`,
-//     changeOrigin: true
-//   }
-// ));
-
-// app.use('/public',
-//     express.static(path.join(__dirname, 'public'),
-//         {
-//             maxAge: '2h'
-//         }));
+app.use(bodyParser.urlencoded({ 'extended': 'true' }));
+app.use(bodyParser.json({ limit: '100mb' }));
 
 //back end logic
-app.post('/value-stream',valueStream.merge);
+app.post('/value-stream',valueStream.calculateTime);
 
+app.get('/test', (req, res) => {
+    res.send("hh");
+    return res;
+})
 
 
 console.log(`${packageInfo.name} ${packageInfo.version} : express starting at ${port}`);
--- value-stream.js
+++ value-stream.js
@@ -1,18 +1,60 @@
-module.exports = {
-    merge:(req,res,next)=>{
+// module.exports = {
+//     merge:(req,res,next)=>{
         
-        let from = req.body.from;
-        let to = req.body.to;
-        let result = copyMandatoryFields(from, to);
-        try{
-           result = merge(from,to);
-        }catch(e){
-           console.error({message:'failed to merge', from:from,to:to,error:e});
+//         let from = req.body.from;
+//         let to = req.body.to;
+//         let result = copyMandatoryFields(from, to);
+//         try{
+//            result = merge(from,to);
+//         }catch(e){
+//            console.error({message:'failed to merge', from:from,to:to,error:e});
+//         }
+//         //saveJsonFile('case.json', {from:from,to:to,result:result});
+//         res.status(200).json(result);
+//     }
+//     ,core: {
+//         merge:merge
+//     }
+// };
+const _ = require('lodash');
+
+module.exports = {
+    calculateTime:(req,res,next)=>{
+        console.log(req.body);
+        
+        let numberUnit = req.body["number-of-units"];
+        // let way = req.body.way;
+        let stations = req.body.stations;
+        let productivityPerSatation = req.body.stations.map(i=>i["seconds-per-unit"]);
+        console.log("productivityPerSatation",productivityPerSatation);
+        if(numberUnit == 1) {
+            res.status(200).json({time: _.sum(productivityPerSatation)});
+            return;
         }
-        //saveJsonFile('case.json', {from:from,to:to,result:result});
-        res.status(200).json(result);
-    }
-    ,core: {
-        merge:merge
+
+        let minProductivity = _.maxBy(productivityPerSatation);
+        console.log("minProductivity",minProductivity);
+
+        let minProductivityOrder = _.find(stations, {'seconds-per-unit':minProductivity}).order;
+        console.log("minProductivityOrder",minProductivityOrder);
+
+        let prefixStations = _
+        .chain(stations)
+        .sortBy('order')
+        .filter((s)=>s.order<minProductivityOrder)
+        .value();
+        let suffixStations = _
+        .chain(stations)
+        .sortBy('order')
+        .filter((s)=>s.order>minProductivityOrder)
+        .value();
+        console.log("prefixStations",prefixStations);
+
+        let timeUsed = _.sum(prefixStations.map(i=>i["seconds-per-unit"])) + numberUnit * minProductivity + _.sum(suffixStations.map(i=>i["seconds-per-unit"]));
+        console.log('timused', timeUsed);
+        // res.send(timeUsed);
+        res.status(200).json(JSON.parse(JSON.stringify({"time": timeUsed})));
+        return res;
+        // return timeUsed;
     }
 };
